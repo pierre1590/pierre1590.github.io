a6d323c0f4e86ca0da5d73d3c4676ab3eaeca17a
Piero Sabino <piero.sa@icloud.com>
2019-05-04 22:02:09 +0200

    update

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 import CardList from './CardList'
-import { robots} from './robots';
 import SearchBox from './SearchBox';
 
 class  App extends Component {
     constructor() {
         super()
             this.state = {
-                robots : robots,
+                robots : [],
                 searchfield : ''
             }
     }
 
+
+    componentDidMount(){
+            fetch('https://jsonplaceholder.typicode.com/users')
+                    .then(response=> response.json())
+                    .then(users=> this.setState({robots: users}));
+    }
     
     onSearchChange = (event) => {
         this.setState({searchfield:event.target.value})
@@ -22,8 +27,8 @@ class  App extends Component {
         })
        
       return (
-        <div className={'tc'}>
-            <h1 className={'f1'}>Robofriends</h1>
+        <div className='tc'>
+            <h1 className='f1'>Robofriends</h1>
             <SearchBox searchChange={this.onSearchChange} />
             <CardList robots={filteredRobots} />
         </div>
